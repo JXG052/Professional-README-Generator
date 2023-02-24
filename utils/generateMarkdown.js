a3c26f8a9bf239b2d62c48518e59336c188bd5a7
JXG052 <jxg0521@gmail.com>
2023-02-24 22:34:45 +0000

    Added ReadMe and organised code into functions

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -4,7 +4,7 @@ function generateMarkdown(data) {
 
   ## Description
   
-  My motivation for this project was to utilise my knowledge of using server API's to build a Weather Dashboard App. The purpose of the App is to provide both current and future forecast for a given City. Previous searches were be to saved and made accessible the buttons. 
+ ${data.description}
   
   ## Table of Contents 
   
@@ -15,28 +15,25 @@ function generateMarkdown(data) {
   
   ## Installation
   
-  n/a
+${data.installation}
   
   ## Usage
   
-  Open the page and enter a City. You will be presented with the current and future forecast. A button will be added to the side of the page, linking to your previous searches
+${data.usage}  
   
+  ![image of deployed site](${data.image})
   
-  ![image of deployed site](/assets/images/screenshot.png)
-  
-  [Link to page](https://jxg052.github.io/Weather-Dashboard/)
-  
+  [Link to page](${data.link})
   
   
   ## Credits
   
-  Thanks to edx bootcamp team for providing starter code and mockup.
+${data.credits}    
+ 
+ ## License
   
-  ## License
-  
-  MIT License
-  
-  Copyright (c) 2023 JXG052
+ ${data.license}  
+
 `;
 }
 
